03d4163a88f24209d7f5dd97d6291cf222f52d0f
Nicholas Rockwell <70916509+grandpaku@users.noreply.github.com>
2021-11-26 16:37:24 -0700

    error_testing

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -16,6 +16,7 @@ import { MemberDetailComponent } from './members/member-detail/member-detail.com
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { ToastrModule } from 'ngx-toastr';
+import { NewComponentComponent } from './errors/new-component/new-component.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { ToastrModule } from 'ngx-toastr';
     MemberListComponent,
     MemberDetailComponent,
     ListsComponent,
-    MessagesComponent
+    MessagesComponent,
+    NewComponentComponent
   ],
   imports: [
     BrowserModule,
